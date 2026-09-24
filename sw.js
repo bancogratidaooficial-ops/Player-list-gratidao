be34e0197ec5f8de4eaa307649b57ee0d44428c6
bancogratidaooficial-ops <bancogratidao.oficial@gmail.com>
2026-09-23 22:14:57 -0300

    Add service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'v86-play-v1';
+const urlsToCache = ['./','./index.html','./manifest.json','./privacidade.html'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(names => Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))));
+});
